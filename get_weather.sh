d28f662f6deaf9c98866ab5f3ee50ec7d8cd081f
Andre Barbosa <andre.jbarbosa@pm.me>
2021-11-07 16:09:29 -0500

    Update.

--- get_weather.sh
+++ get_weather.sh
@@ -4,7 +4,7 @@ while true; do
 
     [ ! -f /tmp/weather_report.tmp ] && touch /tmp/weather_report.tmp
 
-    curl "wttr.in?format=%C+%t" > /tmp/weather_report.tmp \
+    curl "wttr.in?format=%C+%t&?m" > /tmp/weather_report.tmp \
     || echo "..." > /tmp/weather_report.tmp
 
     sleep 300
